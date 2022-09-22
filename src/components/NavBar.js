63b91eb92898147208c7e1b95d129c7c893e6256
Ramiro Alvarez <alvarezramiro05@gmail.com>
2022-09-21 23:44:55 -0300

    Alvarez-Gamerhouse-Entrega1

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+
+import'./NavBar.css'
+
+
+const NavBar = () => {
+    return(
+        <nav class="navbar">
+           
+            <div class="botones">
+                <button class="boton">Productos</button>
+                <button class="boton">Ofertas</button>
+                <button class="boton">Carrito</button>
+                <button class="boton">Sobre Nosotros</button>
+            </div> 
+            <div >
+                <h1 class="barra">
+                    GAMERHOUSE
+                </h1>
+            </div>
+            <div></div>
+        </nav>
+    )
+}
+
+
+export default NavBar
